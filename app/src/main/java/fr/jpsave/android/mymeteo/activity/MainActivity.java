32e809fb1239203ac1c9584c11279bce1b3df1cf
J-Phiz <jp.save@gmail.com>
2021-08-23 11:56:23 +0200

    TP2: Add internet connection check

---
+++ app/src/main/java/fr/jpsave/android/mymeteo/activity/MainActivity.java
@@ -0,0 +1,56 @@
+package fr.jpsave.android.mymeteo.activity;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageView;
+import android.widget.LinearLayout;
+import android.widget.TextView;
+
+import com.google.android.material.floatingactionbutton.FloatingActionButton;
+
+import fr.jpsave.android.mymeteo.R;
+import fr.jpsave.android.mymeteo.tools.Tools;
+
+public class MainActivity extends AppCompatActivity {
+
+    private TextView mTvCityName;
+    private TextView mTvCityDesc;
+    private TextView mTvCityTemp;
+    private ImageView mIvDescIcon;
+    private TextView mTvError;
+    private LinearLayout mLlTempInfos;
+    private FloatingActionButton mFabFavorite;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        mTvCityName = findViewById(R.id.tv_city_name);
+        mTvCityDesc = findViewById(R.id.tv_city_desc);
+        mTvCityTemp = findViewById(R.id.tv_city_temp);
+        mIvDescIcon = findViewById(R.id.iv_desc_icon);
+        mTvError = findViewById(R.id.tv_error);
+        mLlTempInfos = findViewById(R.id.ll_temp_infos);
+        mFabFavorite = findViewById(R.id.favoriteButton);
+
+        if (!Tools.checkInternetConnection(this)) {
+            mTvError.setText(R.string.no_internet);
+            mTvError.setVisibility(View.VISIBLE);
+            mLlTempInfos.setVisibility(View.INVISIBLE);
+            mFabFavorite.setVisibility(View.INVISIBLE);
+        } else {
+            mTvError.setVisibility(View.INVISIBLE);
+            mLlTempInfos.setVisibility(View.VISIBLE);
+            mFabFavorite.setVisibility(View.VISIBLE);
+
+            mTvCityName.setText(R.string.city_name);
+            mTvCityDesc.setText(R.string.city_desc);
+            mTvCityTemp.setText(R.string.city_temp);
+            mIvDescIcon.setImageResource(R.drawable.weather_sunny_white);
+        }
+    }
+
+}
---
+++ app/src/main/java/fr/jpsave/android/mymeteo/tools/Tools.java
@@ -0,0 +1,24 @@
+package fr.jpsave.android.mymeteo.tools;
+
+import android.content.Context;
+import android.net.ConnectivityManager;
+import android.net.NetworkInfo;
+import android.util.Log;
+
+public class Tools {
+
+    public static boolean checkInternetConnection(Context context) {
+        ConnectivityManager cm = (ConnectivityManager) context.getSystemService(Context.CONNECTIVITY_SERVICE);
+        NetworkInfo networkInfo = cm.getActiveNetworkInfo();
+        if (networkInfo != null && networkInfo.isConnected()) {
+            // Oui il y a Internet
+            Log.i(context.getClass().getName(), "Internet Access OK");
+            return true;
+        } else {
+            // Non... J’affiche un message à l’utilisateur
+            Log.e(context.getClass().getName(), "No Internet Access");
+            return false;
+        }
+    }
+
+}
